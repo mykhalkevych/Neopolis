f50f57a7794628eed38379720c8dcc6033233ae1
nazar <nazik542@gmail.com>
2015-09-28 09:32:31 +0300

    responsive main nav

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
   jQuery(document).ready(function($){ 
-/*select*/ 
-$(".select").selectbox(); 
-});
- 
+  	/*select*/ 
+  	$(".select").selectbox(); 
+
+  	$(".letters li").click(function(){
+  		if($(".mobile_select").hasClass("on")){ 
+  			$(".mobile_select").removeClass("on");
+  			$(".mobile_select").text($(this).text());
+  			$(".letters").scrollTop("0");
+  			$(".letters").css({"overflow":"hidden"});
+  			$(".letters").animate({"height":"22px"},300,function(){
+  				console.log("sdfs");
+  			});
+  		}
+  		else{
+  			$(".mobile_select").addClass("on");
+  			$(".letters").css({"overflow-y":"scroll"});
+  			$(".letters").animate({"height":"110px"},300,function(){
+  				console.log("sdfs");
+  			});
+  		}
+
+  	}); 
+
+  });
